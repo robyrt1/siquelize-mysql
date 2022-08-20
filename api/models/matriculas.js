54a950f36b72eb9e2691f6a17af91dfd61d2f2f6
robyrt1 <85000849+robyrt1@users.noreply.github.com>
2022-08-20 10:15:46 -0400

    upload_ultimas_atualizaçoes

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Matriculas extends Model {
-    /**bjb
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       // define association here
       Matriculas.belongsTo(models.Pessoas, {
@@ -24,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Matriculas',
+    paranoid: true
   });
   return Matriculas;
 };
